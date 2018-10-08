0098a09c7379ccbbb335da8116f5b81a2d143d75
Iury Dias Piva <iurypiva@gmail.com>
2018-10-08 20:52:48 -0300

    Update animation.js

--- client/airship/animation.js
+++ client/airship/animation.js
@@ -33,7 +33,7 @@ function getNextSpeed(airship) {
 function animateAirships() {
   airships.forEach(airship => {
     // rotationAnimation(airship)
-    
+
     const nextPosition = getNextPosition(airship)
     getNextSpeed(airship)
     const pixels = coordinatesToPx(nextPosition.x, nextPosition.y)
@@ -69,51 +69,65 @@ function animateAirships() {
 }
 
 // function rotationAnimation(airship) {
-  //   const speed = airship.speed * 1000 // m/s
-  //   const radius = Math.pow(speed, 2) / (9.8 * Math.tan(airship.bankAngle * Math.PI / 180)) // m
-  //   const rateOfTurn = 5.25 * 5 // degrees per second
-  //   const x = airship.x + radius/1000 * (Math.cos(Math.abs(airship.direction + 45 - 360) * Math.PI / 180)),
-  //         y = airship.y + radius/1000 * -(Math.sin(Math.abs(airship.direction + 45 - 360) * Math.PI / 180))
-  //   rotate(airship, { x, y, angle: rateOfTurn / FPS })
-  //   airship.direction = Math.abs((airship.direction + rateOfTurn/FPS) % 360)
+//   const speed = airship.speed * 1000 // m/s
+//   const radius = Math.pow(speed, 2) / (9.8 * Math.tan(airship.bankAngle * Math.PI / 180)) // m
+//   const rateOfTurn = 5.25 * 5 // degrees per second
+//   const x = airship.x + radius/1000 * (Math.cos(Math.abs(airship.direction + 45 - 360) * Math.PI / 180)),
+//         y = airship.y + radius/1000 * -(Math.sin(Math.abs(airship.direction + 45 - 360) * Math.PI / 180))
+//   rotate(airship, { x, y, angle: rateOfTurn / FPS })
+//   airship.direction = Math.abs((airship.direction + rateOfTurn/FPS) % 360)
 // }
 
 function directionToPoint(airship, point) {
   const pointClone = Object.assign({}, point)
   pointClone.x -= airship.x
   pointClone.y -= airship.y
-  return (Math.atan2(pointClone.y, pointClone.x) * 180 / Math.PI)
+  const result = Math.atan2(pointClone.y, pointClone.x) * 180 / Math.PI
+  if(result > 0) {
+    return result
+  } else if (result < 0) {
+    return result + 360
+  } else return 0
 }
 
 function getNextDirection(airship) {
   const rateOfTurn = 5.25 // degrees per second
   if (airship.changeDirectionTo === null) {
     return false
-  } else
-  // VIRANDO SENTINDO ANTIHORÁRIO
-  if (directionToPoint(airship, airship.changeDirectionTo) - airship.direction > 0) {
-    const airshipClone = Object.assign({}, airship)
-    airshipClone.direction += rateOfTurn / FPS
-    if (directionToPoint(airshipClone, airship.changeDirectionTo) - airshipClone.direction <= 0) {
-      airship.direction = directionToPoint(airship, airship.changeDirectionTo)
+  } else {
+    let difference = directionToPoint(airship, airship.changeDirectionTo) - airship.direction
+    if (difference < 0) difference += 360
+
+    if (difference > 0 && difference <= 180) {
+      // VIRANDO SENTINDO ANTIHORÁRIO
+      const airshipClone = Object.assign({}, airship)
+      airshipClone.direction = (airshipClone.direction + rateOfTurn / FPS) % 360
+      difference = directionToPoint(airshipClone, airship.changeDirectionTo) - airshipClone.direction
+      if (difference < 0) difference += 360
+      if (difference == 0 || difference > 359 || difference < 1) {
+        airship.direction = directionToPoint(airship, airship.changeDirectionTo)
+        airship.changeDirectionTo = null
+      } else {
+        airship.direction = airshipClone.direction
+      }
+    } 
+    else
+    if (difference > 180 && difference < 360) {
+      // VIRANDO SENTIDO HORÁRIO
+      const airshipClone = Object.assign({}, airship)
+      airshipClone.direction = (airshipClone.direction - rateOfTurn / FPS) < 0 ? 360 - (airshipClone.direction - rateOfTurn / FPS) : (airshipClone.direction - rateOfTurn / FPS)
+      difference = directionToPoint(airshipClone, airship.changeDirectionTo) - airshipClone.direction
+      if (difference < 0) difference += 360
+      if (difference == 0 || difference > 359 || difference < 1) {
+        airship.direction = directionToPoint(airship, airship.changeDirectionTo)
+        airship.changeDirectionTo = null
+      } else {
+        airship.direction = airshipClone.direction
+      }
+    } else
+    if (directionToPoint(airship, airship.changeDirectionTo) - airship.direction == 0) {
       airship.changeDirectionTo = null
-    } else {
-      airship.direction = airshipClone.direction
-    }
-  } else 
-  // VIRANDO SENTIDO HORÁRIO
-  if (directionToPoint(airship, airship.changeDirectionTo) - airship.direction < 0) {
-    const airshipClone = Object.assign({}, airship)
-    airshipClone.direction -= rateOfTurn / FPS
-    if (directionToPoint(airshipClone, airship.changeDirectionTo) - airshipClone.direction >= 0) {
-      airship.direction = directionToPoint(airship, airship.changeDirectionTo)
-      airship.changeDirectionTo = null
-    } else {
-      airship.direction = airshipClone.direction
-    }
-  } else
-  if (directionToPoint(airship, airship.changeDirectionTo) - airship.direction == 0) {
-    airship.changeDirectionTo = null
+    } 
   }
 }
 
