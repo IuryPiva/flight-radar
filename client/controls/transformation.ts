1e2ccbfba76b4506a8c9385a77e9e49a5bf9eee2
Iury Dias Piva <iurypiva@gmail.com>
2018-12-01 16:37:53 -0200

    fix(transformations): when select more than one airship it should move them individually

--- client/controls/transformation.ts
+++ client/controls/transformation.ts
@@ -8,10 +8,10 @@ import { copyInstance } from "../utils/clone";
 export function addTransformationEventListeners(airships: Airships) {
   document.getElementById('transladar').addEventListener('submit', (event) => {
     event.preventDefault()
-    const point = new Cartesian(getNumberFromInput('transladar-x'),getNumberFromInput('transladar-y'))
-
+    
     airships.getSelected().getAll().forEach(
       (airship: Airship) => {
+        const point = new Cartesian(getNumberFromInput('transladar-x'),getNumberFromInput('transladar-y'))
         point.translate(airship.position)
 
         airship.goTo(point)
@@ -21,10 +21,10 @@ export function addTransformationEventListeners(airships: Airships) {
 
   document.getElementById('escalonar').addEventListener('submit', (event) => {
     event.preventDefault()
-    const point = new Cartesian(getNumberFromInput('escalonar-x'),getNumberFromInput('escalonar-y'))
     
     airships.getSelected().getAll().forEach(
       (airship: Airship) => {
+        const point = new Cartesian(getNumberFromInput('escalonar-x'),getNumberFromInput('escalonar-y'))
         point.scale(airship.position)
 
         airship.goTo(point)
@@ -34,11 +34,11 @@ export function addTransformationEventListeners(airships: Airships) {
 
   document.getElementById('rotacionar').addEventListener('submit', (event) => {
     event.preventDefault()
-    const point = new Cartesian(getNumberFromInput('rotacionar-x'),getNumberFromInput('rotacionar-y'))
-    const angle = new Degrees(getNumberFromInput('rotacionar-angulo'))
     
     airships.getSelected().getAll().forEach(
       (airship: Airship) => {
+        const point = new Cartesian(getNumberFromInput('rotacionar-x'),getNumberFromInput('rotacionar-y'))
+        const angle = new Degrees(getNumberFromInput('rotacionar-angulo'))
         const resultantPosition: Cartesian = copyInstance(airship.position)
         resultantPosition.reduce(point)
         resultantPosition.rotate(angle)
